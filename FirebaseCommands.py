041a2533e8348490467be95c84f454ba7ab34f02
egeyelken <49537772+egeyelken@users.noreply.github.com>
2021-04-10 21:25:42 +0300

    Update FirebaseCommands.py

--- FirebaseCommands.py
+++ FirebaseCommands.py
@@ -2,7 +2,6 @@ import pyrebase
 import json
 import collections
 
-
 firebaseConfig = {'apiKey': "AIzaSyC5gsWS4yRMLoKftc-rEhYOj9XnZeFC-1Y",
                   'authDomain': "campustakeaway-fadb9.firebaseapp.com",
                   'databaseURL': "https://campustakeaway-fadb9-default-rtdb.firebaseio.com",
@@ -16,6 +15,7 @@ db = firebase.database()
 auth = firebase.auth()
 storage = firebase.storage()
 
+## Work Package 1: Signup and Login ##
 
 json = {"username": "aratello",
         "phone_number": 11112222333,
@@ -27,7 +27,7 @@ json = {"username": "aratello",
         "gender": 'female',
         "date_of_birth": "11.03.1998"}
 
-def signup_customer(json_info):  # for Students/Staff
+def signup_customer(json_info):
     username = json_info['username']
     phone_number = json_info['phone_number']
     email = json_info['email']
@@ -90,13 +90,13 @@ def login(email, password):
     except:
         print('Invalid mail or password.')
 
-def update(username, title, newval): ###will be updated for username
-    db.child('Users').child('Customers').child(username).update({title: newval})
+def update(username, attribute, newval):
+    db.child('Users').child('Customers').child(username).update({attribute: newval})
 
-def delete_user(username): ###will be updated
+def delete_user(username):
     db.child('Users').child('Customers').child(username).remove()
 
-def get_user_by_mail(username): ###will be updated
+def get_user_by_username(username):
     people = db.child('Users').child('Customers').get()
     for p in people.each():
         if p.key() == username:
@@ -109,22 +109,37 @@ def get_all_services(university):
         if cafe.val()['university'] == university:
             dict1[cafe.key()] = cafe.val()
     return dict1
-#########################################################################
+
+
+## Work Package 2: Menu and Basket ##
 
 json3 = {"product_id": "NEROFK1",
          "name": "Filter Coffee",
          "price": 8.0,
          "product_description": "Fresh Filter Coffee",
+         "additional_notes": "No-Milk"}
+json4 = {"product_id": "NEROFK2",
+         "name": "Filter Coffee with Milk",
+         "price": 10.0,
+         "product_description": "Fresh Filter Coffee with Milk",
          "additional_notes": "Soy-Milk"}
 
+#Menu
 def add_to_menu(json, username):
     db.child('Users').child('Services').child(username).child('Menu').child(json['name']).set(json)
+
+def get_menu(username):
+    return db.child('Users').child('Services').child(username).child('Menu').get().val()
 
 def remove_from_menu(name, username):
     db.child('Users').child('Services').child(username).child('Menu').child(name).remove()
 
+#Basket
 def add_to_basket(json, username):
     db.child('Users').child('Customers').child(username).child('Basket').child(json['name']).set(json)
+
+def get_basket(username):
+    return db.child('Users').child('Customers').child(username).child('Basket').get().val()
 
 def drop_from_basket(name, username):
     db.child('Users').child('Customers').child(username).child('Basket').child(name).remove()
@@ -132,36 +147,22 @@ def drop_from_basket(name, username):
 def empty_basket(username): #call after empty_basket/payment_complete/new_provider
     db.child('Users').child('Customers').child(username).child('Basket').remove()
 
-def get_basket(username): ######################
-    return db.child('Users').child('Customers').child('aratello').child('Basket').get().val().values()
+def calculate_total(username):
+    total = 0.0
+    items = db.child('Users').child('Customers').child(username).child('Basket').get()
+    for i in items.each():
+        total = total + i.val()['price']
+    return total
 
-def get_menu(username): #######################
-    return db.child('Users').child('Services').child(username).child('Menu').get().val()
 
-print(get_menu('DeParis'))
+## Work Package 3: Timeslots & Tracking ##
 
-############################
 def define_timeslots(username, opening_hour, closing_hour, limit):
     number_of_timeslots = (closing_hour - opening_hour) * 2
-    data = {
-    'timeslot1': limit,
-    'timeslot2': limit,
-    'timeslot3': limit,
-    'timeslot4': limit,
-    'timeslot5': limit,
-    'timeslot6': limit,
-    'timeslot7': limit,
-    'timeslot8': limit,
-    'timeslot9': limit,
-    'timeslot10': limit,
-    'timeslot11': limit,
-    'timeslot12': limit,
-    'timeslot13': limit,
-    'timeslot14': limit,
-    'timeslot15': limit,
-    'timeslot16': limit,
-    'timeslot17': limit,
-    'timeslot18': limit}
+    data = {}
+    for i in range(number_of_timeslots):
+        k = 'timeslot' + str(i+1)
+        data[k] = limit
 
     db.child('Users').child('Services').child(username).child('Timeslots').set(data)
 
@@ -177,23 +178,33 @@ def decrease_availabile_slot(username, index):
     if newval >= 0:
       db.child('Users').child('Services').child(username).child('Timeslots').update({"timeslot" + str(index): newval})
 
+def add_to_active_orders(company, username, timeslot):
+    basket = get_basket(username)
+    db.child('Users').child('Services').child(company).child('Active Orders').child(username).set(basket)
+    decrease_availabile_slot(company, timeslot)
+    #send_new_order_notification()
 
-#decrease_availabile_slot("DeParis", 9)
-#############TESTS######################
+def order_ready(company, username):
+    db.child('Users').child('Services').child(company).child('Active Orders').child(username).remove()
+    #send_ready_notification()
+
+############# TESTS #############
 #signup_customer(json)
 # login('dummy@ku.edu.tr','123456')
 # update('dummy@ku.edu.tr', 'first name', 'Ege')
 # update('dummy@ku.edu.tr', 'last name', 'Yelken')
-# print(get_user_by_mail('dummy@ku.edu.tr'))
+#print(get_user_by_username('aratello'))
 #print(get_all_services('Koc University'))
 #signup_service(json2)
 #add_to_menu(json3, "DeParis")
 #remove_from_menu('Filter Coffee', 'DeParis')
-#add_to_basket(json3, "aratello")
-###print(get_basket("aratello"))
+#add_to_basket(json4, "aratello")
 #drop_from_basket('Filter Coffee', 'aratello')
 #empty_basket('aras')
+#print(calculate_total('aratello'))
 #print(get_basket('aratello'))
+#print(get_menu('DeParis'))
 #define_timeslots("DeParis", 8, 17, 5)
-
-
+#decrease_availabile_slot("DeParis", 9)
+#add_to_active_orders('DeParis', 'aratello', 5)
+#order_ready('DeParis', 'aratello')
